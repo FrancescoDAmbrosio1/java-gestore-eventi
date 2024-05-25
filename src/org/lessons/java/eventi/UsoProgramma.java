b1702293d06800793d9a59a7a8c2717b7dbc569b
FrancescoDAmbrosio1 <158843577+FrancescoDAmbrosio1@users.noreply.github.com>
2024-05-25 14:25:06 +0200

    Update UsoProgramma.java

--- src/org/lessons/java/eventi/UsoProgramma.java
+++ src/org/lessons/java/eventi/UsoProgramma.java
@@ -1,6 +1,7 @@
 package org.lessons.java.eventi;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class UsoProgramma {
@@ -9,9 +10,12 @@ public class UsoProgramma {
 		int scelta;
 		String titolo = null;
 		String data;
+		String oraEvento;
+		double prezzo;
 		int postiTotali = 0;
 		int postiDaPrenotare;
 		int postiDaDisdire;
+		Evento evento = null;
 		
 		Scanner scan = new Scanner(System.in);
 		
@@ -43,16 +47,46 @@ public class UsoProgramma {
 					}
 					ProgrammEventi listaEventi = new ProgrammEventi(eventoCreato.titolo);
 					listaEventi.aggiungiEvento(eventoCreato);
+					evento = eventoCreato;
 					System.out.println("Hai inserito il seguente evento: " + eventoCreato.toString());
 			
 					break;
 					
 				case 2:
+					System.out.println("Inserisci il titolo dell'evento: ");
+					//da problemi con stringhe formate da più parole
+					titolo =scan.next();
+					System.out.println("Inserisci data evento nel formato yyyy-mm-gg: ");
+					data = scan.next();
+					System.out.println("Inserisci i posti totali disponibili per l'evento: ");
+					postiTotali = scan.nextInt();
+					System.out.println("Inserisci orario evento nel formato hh:mm ");
+					oraEvento = scan.next();
+					System.out.println("Inserisci prezzo ticket evento (xx,xx): ");
+					prezzo = scan.nextDouble();
+					Evento concertoCreato = new Concerto(titolo, LocalDate.parse(data), postiTotali, 0,LocalTime.parse(oraEvento), prezzo);
+					if(concertoCreato.setData(data)) {
+						do {
+							System.out.println("Inserisci nuova data evento nel formato yyyy-mm-gg: ");
+							data = scan.next();
+							concertoCreato.data = LocalDate.parse(data);
+							} while(concertoCreato.setData(data));
+					}
+					evento = concertoCreato;
+					System.out.println("Hai inserito il seguente evento: " + concertoCreato.toString());
+					break;
+					
+				case 3: 
+					Evento.menuGestioneEvento();
+					break;
+					
+				case 4:
 					System.out.println("------ PASSA ALLA GESTIONE EVENTI ------");
 					break;
 					
 			}
-				
+			ProgrammEventi listaEventi = new ProgrammEventi(titolo);
+			listaEventi.aggiungiEvento(evento);	
 		}while(scelta != 2);
 		
 		
